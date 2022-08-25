509890c58bd716473cb057cc77acb6dc2d5acd17
Brett Kuhn <brettkuhn87@gmail.com>
2022-08-25 12:11:08 -0600

    Initial commit, completed in-class functions-test

--- functions.js
+++ functions.js
@@ -1,9 +1,35 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+
+  return num1 + num2
+
+}
+
+
+function sayHello(name) {
+  if (name === "Will") {
+    return `No more testing ${name}!`
+  } else {
+    return `Hi there ${name}!`
+}
+}
+
+
+function buildCar(color, type) {
+  var parameters = color, type;
+  var vehicle = {
+    color: color,
+    type: type,
+  }
+  if (parameters === undefined) {
+    return {}
+  } else {
+    return vehicle
+  }
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar,
 }
